5e1709174fb3df9702ef1310c71ea44f00abc084
Aleksander Brymora <aleksander.australia@gmail.com>
2020-11-09 22:29:44 +1100

    small refactor to tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,36 +1,47 @@
 import { Pacman } from './Pacman';
 
-it('Creates an instance of Pacman', () => {
-	const p = new Pacman();
-	expect(p).not.toBeUndefined();
-});
+describe('Unit tests for each method', () => {
+	let p: Pacman;
+	let consoleSpy: jest.SpyInstance;
 
-it('Logs the position of placement after being placed', () => {
-	const consoleSpy = jest.spyOn(console, 'log');
-	const p = new Pacman();
-	p.place(0, 0, 'north').report();
-	expect(consoleSpy).toHaveBeenCalledWith('0,0,NORTH');
-});
+	beforeEach(() => {
+		consoleSpy = jest.spyOn(console, 'log');
+		p = new Pacman();
+	});
 
-it('Changes the direction after calling left and right method', () => {
-	const consoleSpy = jest.spyOn(console, 'log');
-	const p = new Pacman();
-	p.place(0, 0, 'north').left().report();
-	expect(consoleSpy).toHaveBeenCalledWith('0,0,WEST');
-	p.right().report();
-	expect(consoleSpy).toHaveBeenCalledWith('0,0,NORTH');
-});
+	it('Creates an instance of Pacman', () => {
+		expect(p).not.toBeUndefined();
+	});
 
-it('Moves by one tile', () => {
-	const consoleSpy = jest.spyOn(console, 'log');
-	const p = new Pacman();
-	p.place(0, 0, 'north').move().report();
-	expect(consoleSpy).toHaveBeenCalledWith('0,1,NORTH');
-});
+	it("Doesn't let you do anything unless you placed Pacman", () => {
+		const notPlaced = "Can't do that. You have't placed the Pacman yet :/";
+		p.left();
+		expect(consoleSpy).toHaveBeenCalledWith(notPlaced);
+		p.move();
+		expect(consoleSpy).toHaveBeenCalledWith(notPlaced);
+		p.report();
+		expect(consoleSpy).toHaveBeenCalledWith(notPlaced);
+	});
 
-it("Doesn't move if it's on the edge", () => {
-	const consoleSpy = jest.spyOn(console, 'log');
-	const p = new Pacman();
-	p.place(0, 0, 'north').left().move().report();
-	expect(consoleSpy).toHaveBeenCalledWith('0,0,WEST');
+	it('Logs the position of placement after being placed', () => {
+		p.place(0, 0, 'north').report();
+		expect(consoleSpy).toHaveBeenCalledWith('0,0,NORTH');
+	});
+
+	it('Changes the direction after calling left and right method', () => {
+		p.place(0, 0, 'north').left().report();
+		expect(consoleSpy).toHaveBeenCalledWith('0,0,WEST');
+		p.right().report();
+		expect(consoleSpy).toHaveBeenCalledWith('0,0,NORTH');
+	});
+
+	it('Moves by one tile', () => {
+		p.place(0, 0, 'north').move().report();
+		expect(consoleSpy).toHaveBeenCalledWith('0,1,NORTH');
+	});
+
+	it("Doesn't move if it's on the edge", () => {
+		p.place(0, 0, 'north').left().move().report();
+		expect(consoleSpy).toHaveBeenCalledWith('0,0,WEST');
+	});
 });
